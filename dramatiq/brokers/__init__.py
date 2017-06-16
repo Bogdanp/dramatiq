8c2b7de9c7dcf1f25fa4bf3f2b334994cf3e453f
Bogdan Popa <popa.bogdanp@gmail.com>
2017-06-16 10:58:10 +0300

    refactor: fail gracefully if rabbitmq support isn't vailable

--- dramatiq/brokers/__init__.py
+++ dramatiq/brokers/__init__.py
@@ -1,2 +1,11 @@
-from .rabbitmq import RabbitmqBroker  # noqa
+import warnings
+
 from .stub import StubBroker  # noqa
+
+try:
+    from .rabbitmq import RabbitmqBroker  # noqa
+except ImportError as e:
+    warnings.warn(
+        "RabbitmqBroker not available.  `pip install dramatiq[rabbitmq]` to get RabbitMQ support.",
+        category=RuntimeWarning, stacklevel=2,
+    )
