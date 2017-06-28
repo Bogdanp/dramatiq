5c9569d006f85c586dd5d8e3551b801abb5602d8
Bogdan Popa <popa.bogdanp@gmail.com>
2017-06-28 13:42:03 +0300

    chore: update benchmarks

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -1,7 +1,4 @@
-# Celery vs Dramatiq latency benchmark
-
-This benchmark compares how well Dramatiq and Celery fare when running
-some number of latency-bound tasks.
+# Celery vs Dramatiq benchmarks
 
 ## Setup
 
@@ -11,16 +8,18 @@ some number of latency-bound tasks.
 1. Run `redis-server` or `rabbitmq-server` in a terminal window.
 1. In a separate terminal window, run `memcached`.
 
-## Running the benchmark
+## Running the benchmarks
 
-Run `python latency.py` to run the Dramatiq benchmark, and `python
-latency.py --use-celery` to run the Celery benchmark.  Prepend `env
+Run `python bench.py` to run the Dramatiq benchmark, and `python
+bench.py --use-celery` to run the Celery benchmark.  Prepend `env
 REDIS=1` to each command to run the benchmarks against Redis.
+
+Run `python bench.py --help` to see all the available options.
 
 ## Caveats
 
 As with any benchmark, take it with a grain of salt.  Dramatiq has an
-advantage over Celery here by design (workers are multi-threaded).
+advantage over Celery in most of these benchmarks by design.
 
 
 [memcached]: https://memcached.org
---
+++ benchmarks/bench.py
@@ -0,0 +1,147 @@
+import argparse
+import celery
+import dramatiq
+import logging
+import os
+import pylibmc
+import random
+import sys
+import subprocess
+import time
+
+from dramatiq.brokers.redis import RedisBroker
+from dramatiq.brokers.rabbitmq import RabbitmqBroker
+
+logger = logging.getLogger("example")
+counter_key = "latench-bench-counter"
+memcache_client = pylibmc.Client(["localhost"], binary=True)
+memcache_pool = pylibmc.ClientPool(memcache_client, 8)
+random.seed(1337)
+
+if os.getenv("REDIS") == "1":
+    broker = RedisBroker()
+    dramatiq.set_broker(broker)
+    celery_app = celery.Celery(broker="redis:///")
+
+else:
+    broker = RabbitmqBroker()
+    dramatiq.set_broker(broker)
+    celery_app = celery.Celery(broker="amqp:///")
+
+
+def fib_bench(n):
+    p, q = 0, 1
+    while n > 0:
+        p, q = q, p + q
+        n -= 1
+
+    with memcache_pool.reserve() as client:
+        client.incr(counter_key)
+
+    return p
+
+
+def latency_bench():
+    p = random.randint(1, 100)
+    if p == 1:
+        duration = 10
+    elif p <= 30:
+        duration = 5
+    elif p <= 50:
+        duration = 3
+    else:
+        duration = 1
+
+    time.sleep(duration)
+    with memcache_pool.reserve() as client:
+        client.incr(counter_key)
+
+
+dramatiq_fib_bench = dramatiq.actor(fib_bench)
+dramatiq_latency_bench = dramatiq.actor(latency_bench)
+
+celery_fib_bench = celery_app.task(name="fib-bench")(fib_bench)
+celery_latency_bench = celery_app.task(name="latency-bench")(latency_bench)
+
+
+def benchmark_arg(value):
+    benchmarks = ("fib", "latency")
+    if value not in benchmarks:
+        raise argparse.ArgumentTypeError(f"benchmark must be one of {benchmarks!r}")
+    return value
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--benchmark", help="the benchmark to run",
+        type=benchmark_arg, default="latency",
+    )
+    parser.add_argument(
+        "--count", help="the number of messages to benchmark with",
+        type=int, default=10000,
+    )
+    parser.add_argument(
+        "--use-green-threads", help="run workers with green threads rather than system threads",
+        action="store_true", default=False,
+    )
+    parser.add_argument(
+        "--use-celery", help="run the benchmark under Celery",
+        action="store_true", default=False,
+    )
+    return parser.parse_args()
+
+
+def main(args):
+    args = parse_args()
+    for _ in range(args.count):
+        if args.use_celery:
+            if args.benchmark == "latency":
+                celery_latency_bench.delay()
+
+            elif args.benchmark == "fib":
+                celery_fib_bench.delay(random.randint(1, 200))
+
+        else:
+            if args.benchmark == "latency":
+                dramatiq_latency_bench.send()
+
+            elif args.benchmark == "fib":
+                dramatiq_fib_bench.send(random.randint(1, 200))
+
+    print("Done enqueing messages. Booting workers...")
+    with memcache_pool.reserve() as client:
+        client.set(counter_key, 0)
+
+        start_time = time.time()
+        if args.use_celery:
+            subprocess_args = ["celery", "worker", "-A", "bench.celery_app"]
+            if args.use_green_threads:
+                subprocess_args.extend(["-P", "eventlet", "-c", "2000"])
+            else:
+                subprocess_args.extend(["-c", "8"])
+
+        else:
+            if args.use_green_threads:
+                subprocess_args = ["dramatiq", "bench", "-p", "8"]
+
+            else:
+                subprocess_args = ["dramatiq-gevent", "bench", "-p", "8", "-t", "250"]
+
+        proc = subprocess.Popen(subprocess_args)
+        processed = 0
+        while processed < args.count:
+            processed = client.get(counter_key)
+            print(f"{processed}/{args.count} messages processed\r", end="")
+            time.sleep(0.1)
+
+        duration = time.time() - start_time
+        proc.terminate()
+        proc.wait()
+
+    print(f"Took {duration} seconds to process {args.count} messages.")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
