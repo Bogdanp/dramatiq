a94b16e1b63019a5069b4a7738c7b6a19acfff44
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-10 12:46:17 +0200

    chore: skip coverage for import errors

--- dramatiq/rate_limits/backends/__init__.py
+++ dramatiq/rate_limits/backends/__init__.py
@@ -2,7 +2,7 @@ import warnings
 
 try:
     from .memcached import MemcachedBackend  # noqa
-except ImportError:
+except ImportError:  # pragma: no cover
     warnings.warn(
         "MemcachedBackend is not available.  Run `pip install dramatiq[memcached]` "
         "to add support for that backend.", ImportWarning,
@@ -10,7 +10,7 @@ except ImportError:
 
 try:
     from .redis import RedisBackend  # noqa
-except ImportError:
+except ImportError:  # pragma: no cover
     warnings.warn(
         "RedisBackend is not available.  Run `pip install dramatiq[redis]` "
         "to add support for that backend.", ImportWarning,
