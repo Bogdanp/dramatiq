c91c496eaacd21f73f4a890ca727022fa7610aa7
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-24 09:50:01 +0200

    test: bump cli test timeouts to 30s for travis

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,7 @@ def test_cli_fails_to_start_given_an_invalid_broker_name(start_cli):
     # Given that this module doesn't define a broker called "idontexist"
     # When I start the cli and point it at that broker
     proc = start_cli("tests.test_cli:idontexist", stdout=PIPE, stderr=STDOUT)
-    proc.wait(5)
+    proc.wait(30)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
@@ -20,7 +20,7 @@ def test_cli_fails_to_start_given_an_invalid_broker_instance(start_cli):
     # Given that this module defines a "fakebroker" variable that's not a Broker
     # When I start the cli and point it at that broker
     proc = start_cli("tests.test_cli:fakebroker", stdout=PIPE, stderr=STDOUT)
-    proc.wait(5)
+    proc.wait(30)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
