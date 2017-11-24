4958e90d7da218fb47341103083bd1189ebf1bf5
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-24 09:46:15 +0200

    doc: add results example

---
+++ examples/results/README.md
@@ -0,0 +1,17 @@
+# Dramatiq Results Example
+
+This example demonstrates how to use Dramatiq actors to store and
+retrieve task results.
+
+## Running the Example
+
+1. Install [RabbitMQ][rabbitmq] and [Redis][redis]
+1. Install dramatiq: `pip install dramatiq[rabbitmq,redis]`
+1. Run RabbitMQ: `rabbitmq-server`
+1. Run Redis: `redis-server`
+1. In a separate terminal window, run the workers: `env PYTHONPATH=. dramatiq example`.
+1. In another terminal, run `python -m example`.
+
+
+[rabbitmq]: https://www.rabbitmq.com
+[redis]: https://redis.io
---
+++ examples/results/example.py
@@ -0,0 +1,41 @@
+import argparse
+import dramatiq
+import random
+import sys
+import time
+
+from dramatiq.brokers.rabbitmq import RabbitmqBroker
+from dramatiq.results.backends import RedisBackend
+from dramatiq.results import Results
+
+result_backend = RedisBackend()
+broker = RabbitmqBroker()
+broker.add_middleware(Results(backend=result_backend))
+dramatiq.set_broker(broker)
+
+
+@dramatiq.actor(store_results=True)
+def sleep_then_add(t, x, y):
+    time.sleep(t)
+    return x + y
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("count", type=int, help="the number of messages to enqueue")
+    args = parser.parse_args()
+
+    messages = []
+    for _ in range(args.count):
+        messages.append(sleep_then_add.send(
+            random.randint(1, 5),
+            random.randint(0, 1000),
+            random.randint(0, 1000)
+        ))
+
+    for message in messages:
+        print(message.get_result(block=True))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
