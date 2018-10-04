741bbc2f56f6bedf581d1dc127cb08db29e68c2d
Ryan Smith <43353432+ryansm1@users.noreply.github.com>
2018-10-04 09:33:20 -0500

    Handle SIGHUP only on POSIX platforms

--- dramatiq/main.py
+++ dramatiq/main.py
@@ -397,12 +397,17 @@ def main():  # noqa
 
     def sighandler(signum, frame):
         nonlocal reload_process, worker_processes
-        reload_process = signum == signal.SIGHUP
-        signum = {
+        sigmap = {
             signal.SIGINT: signal.SIGTERM,
             signal.SIGTERM: signal.SIGTERM,
-            signal.SIGHUP: signal.SIGHUP,
-        }[signum]
+        }
+        if hasattr(signal, "SIGHUP"):
+            reload_process = signum == signal.SIGHUP
+            sigmap.update({signal.SIGHUP, signal.SIGHUP})
+        else:
+            # Windows can't handle SIGHUP so process reloading isn't supported
+            reload_process = False
+        signum = sigmap[signum]
 
         logger.info("Sending %r to worker processes...", signum.name)
         for proc in worker_processes:
