70c1f52717248cd99028b6747b47d61ef60bc6e2
Antoine Rabany <arabany@wiremind.fr>
2018-10-23 16:15:50 +0200

    fix(test_cli) on travis

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,4 @@
-from subprocess import PIPE, STDOUT
+from subprocess import PIPE
 
 fakebroker = object()
 
@@ -6,24 +6,24 @@ fakebroker = object()
 def test_cli_fails_to_start_given_an_invalid_broker_name(start_cli):
     # Given that this module doesn't define a broker called "idontexist"
     # When I start the cli and point it at that broker
-    proc = start_cli("tests.test_cli:idontexist", stdout=PIPE, stderr=STDOUT)
+    proc = start_cli("tests.test_cli:idontexist", stdout=PIPE, stderr=PIPE)
     proc.wait(5)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
 
     # And the output should contain an error
-    assert b"Module 'tests.test_cli' does not define a 'idontexist' variable." in proc.stdout.read()
+    assert b"Module 'tests.test_cli' does not define a 'idontexist' variable." in proc.stderr.read()
 
 
 def test_cli_fails_to_start_given_an_invalid_broker_instance(start_cli):
     # Given that this module defines a "fakebroker" variable that's not a Broker
     # When I start the cli and point it at that broker
-    proc = start_cli("tests.test_cli:fakebroker", stdout=PIPE, stderr=STDOUT)
+    proc = start_cli("tests.test_cli:fakebroker", stdout=PIPE, stderr=PIPE)
     proc.wait(5)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
 
     # And the output should contain an error
-    assert b"Variable 'fakebroker' from module 'tests.test_cli' is not a Broker." in proc.stdout.read()
+    assert b"Variable 'fakebroker' from module 'tests.test_cli' is not a Broker." in proc.stderr.read()
