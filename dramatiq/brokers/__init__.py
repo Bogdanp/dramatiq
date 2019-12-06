c7d6d24bb3f590bacb1ba70506ef7e34115493ee
Evstrat_BG <evstrat.bg@mail.ru>
2019-12-06 13:52:50 +0300

    beautify imports

--- dramatiq/brokers/__init__.py
+++ dramatiq/brokers/__init__.py
@@ -14,3 +14,13 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from .rabbitmq import RabbitmqBroker
+from .redis import RedisBroker
+from .stub import StubBroker
+
+__all__ = [
+    'RabbitmqBroker',
+    'RedisBroker',
+    'StubBroker'
+]
