9516b4dcb48edafdc1e8d208ad7aa313cba31576
Bogdan Popa <bogdan@defn.io>
2020-03-29 13:06:51 +0300

    tests: re-enable cli tests in CI and increment wait times

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 from subprocess import PIPE, STDOUT
 
-from .common import skip_in_ci
+from .common import skip_on_windows
 
 fakebroker = object()
 
@@ -9,12 +9,12 @@ class BrokerHolder:
     fakebroker = object()
 
 
-@skip_in_ci
+@skip_on_windows
 def test_cli_fails_to_start_given_an_invalid_broker_name(start_cli):
     # Given that this module doesn't define a broker called "idontexist"
     # When I start the cli and point it at that broker
     proc = start_cli("tests.test_cli:idontexist", stdout=PIPE, stderr=STDOUT)
-    proc.wait(5)
+    proc.wait(60)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
@@ -23,12 +23,12 @@ def test_cli_fails_to_start_given_an_invalid_broker_name(start_cli):
     assert b"Module 'tests.test_cli' does not define a 'idontexist' variable." in proc.stdout.read()
 
 
-@skip_in_ci
+@skip_on_windows
 def test_cli_fails_to_start_given_an_invalid_broker_instance(start_cli):
     # Given that this module defines a "fakebroker" variable that's not a Broker
     # When I start the cli and point it at that broker
     proc = start_cli("tests.test_cli:fakebroker", stdout=PIPE, stderr=STDOUT)
-    proc.wait(5)
+    proc.wait(60)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
@@ -37,12 +37,12 @@ def test_cli_fails_to_start_given_an_invalid_broker_instance(start_cli):
     assert b"'tests.test_cli:fakebroker' is not a Broker." in proc.stdout.read()
 
 
-@skip_in_ci
+@skip_on_windows
 def test_cli_fails_to_start_given_an_invalid_nested_broker_instance(start_cli):
     # Given that this module defines a "BrokerHolder.fakebroker" variable that's not a Broker
     # When I start the cli and point it at that broker
     proc = start_cli("tests.test_cli:BrokerHolder.fakebroker", stdout=PIPE, stderr=STDOUT)
-    proc.wait(5)
+    proc.wait(60)
 
     # Then the process return code should be 2
     assert proc.returncode == 2
