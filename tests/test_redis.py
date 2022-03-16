0847b26df807244c69406b52610e97ad2528d7e3
Federico Caselli <cfederico87@gmail.com>
2022-03-16 22:10:22 +0100

    test: update test for issue 480.

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -496,40 +496,28 @@ def test_redis_join_race_condition(redis_broker, redis_worker):
     """
     test for issue https://github.com/Bogdanp/dramatiq/issues/480
 
-    this test was triggered a race condition when ``qsize`` returned only the size of the
-    requested queue, not also of the delay queue.
+    this test verifies that do_qsize counts all messages in the queue and delay queue at the same
+    time, removing the race condition mentioned in the issue
     """
-    old_qsize = redis_broker.do_qsize
-    events = defaultdict(threading.Event)
-
-    class M(dramatiq.Middleware):
-        def before_enqueue(self, broker, message, delay):
-            if not delay:
-                events["default"].set()
-                events["default_done"].wait(2)
-
-        def after_ack(self, broker, message):
-            if message.options.get("eta"):
-                events[dq_name("default")].set()
-
-    def qsize(qn):
-        events[qn].wait(2)
-        events[qn].clear()
-        res = old_qsize(qn)
-        events[f"{qn}_done"].set()
-        return res
-
-    redis_broker.add_middleware(M())
-    redis_broker.do_qsize = qsize
     called = False
+    size = []
 
     @dramatiq.actor
     def go():
+        go_again.send_with_options(delay=50)
+        size.append(redis_broker.do_qsize('default'))  # go ack + go msg + go_again msg
+        size.append(redis_broker.do_qsize(dq_name('default')))  # does the same
+
+    @dramatiq.actor
+    def go_again():
         nonlocal called
         called = True
+        size.append(redis_broker.do_qsize('default'))  # go_again msg + go_again ack
+        size.append(redis_broker.do_qsize(dq_name('default')))  # does the same
 
-    go.send_with_options(delay=50)
+    go.send()
     redis_broker.join("default")
     redis_worker.join()
 
     assert called
+    assert size == [3, 3, 2, 2]
