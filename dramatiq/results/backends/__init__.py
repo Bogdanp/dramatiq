c9104a061a5c70526d89a279c5b85e11040f9c3f
Dan Cardin <ddcardin@gmail.com>
2022-09-27 14:29:52 -0400

    fix: Avoid warning on missing dependencies when those dependencies are not used.
    Defer imports of backend client libraries until the backend is
    instantiated. This should maintain the existing failure behavior which
    lazily causes usages of missing backends to fail. However it defers the
    emission of warnings on unused backends **until** they're used.

--- dramatiq/results/backends/__init__.py
+++ dramatiq/results/backends/__init__.py
@@ -19,20 +19,46 @@ import warnings
 
 from .stub import StubBackend
 
-try:
-    from .memcached import MemcachedBackend
-except ImportError:  # pragma: no cover
-    warnings.warn(
-        "MemcachedBackend is not available.  Run `pip install dramatiq[memcached]` "
-        "to add support for that backend.", ImportWarning,
-    )
-
-try:
-    from .redis import RedisBackend
-except ImportError:  # pragma: no cover
-    warnings.warn(
-        "RedisBackend is not available.  Run `pip install dramatiq[redis]` "
-        "to add support for that backend.", ImportWarning,
-    )
-
 __all__ = ["StubBackend", "MemcachedBackend", "RedisBackend"]
+
+
+def __getattr__(name):
+    module_importer = _module_importers.get(name)
+    if module_importer is None:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+    return module_importer()
+
+
+def import_memcached():
+    try:
+        from .memcached import MemcachedBackend
+
+        return MemcachedBackend
+    except ModuleNotFoundError:
+        warning = ImportWarning(
+            "MemcachedBackend is not available.  Run `pip install dramatiq[memcached]` "
+            "to add support for that backend.",
+        )
+        warnings.warn(warning)
+        raise
+
+
+def import_redis():
+    try:
+        from .redis import RedisBackend
+
+        return RedisBackend
+    except ModuleNotFoundError:
+        warning = ImportWarning(
+            "RedisBackend is not available.  Run `pip install dramatiq[redis]` "
+            "to add support for that backend.",
+        )
+        warnings.warn(warning)
+        raise
+
+
+_module_importers = {
+    "MemcachedBackend": import_memcached,
+    "RedisBackend": import_redis,
+}
