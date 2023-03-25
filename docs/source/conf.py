77d1e5c272dda584daee73f2c8270c287e9b40a8
Bogdan Popa <bogdan@defn.io>
2023-03-25 12:16:26 +0300

    doc: fixup pika links, language [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,7 @@ scv_root_ref = 'v' + release
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -193,6 +193,6 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/3/': None,
-    'https://pika.readthedocs.io/en/1.0.0/': None,
+    'https://pika.readthedocs.io/en/stable/': None,
     'https://redis-py.readthedocs.io/en/latest/': None,
 }
