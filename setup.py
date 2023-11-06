2a1c6a8a07930dec607a7881cd43e96120ee2b23
Finn Lidbetter <finnlidbetter@gmail.com>
2023-11-06 01:10:33 -0500

    Add build support for python3.12, drop python3.7 (#587)

--- setup.py
+++ setup.py
@@ -120,11 +120,11 @@ setup(
     entry_points={"console_scripts": ["dramatiq = dramatiq.__main__:main"]},
     scripts=["bin/dramatiq-gevent"],
     classifiers=[
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: System :: Distributed Computing",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
