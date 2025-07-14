Bogdanp/dramatiq

commit b372f431ae40ff383a5b450dc37c8e5d5671bf49

fix: make sure RabbitMQ broker’s enqueue() attempts the correct number of preset retries (#669)

dramatiq/brokers/rabbitmq.py
@@ -246,7 +246,7 @@ class RabbitmqBroker(Broker):
             self._ensure_queue(queue_name)
 
     def _ensure_queue(self, queue_name):
-        attempts = 1
+        attempts = 0
         while True:
             try:
                 if queue_name in self.queues_pending:
@@ -321,7 +321,7 @@ class RabbitmqBroker(Broker):
                 },
             )
 
-        attempts = 1
+        attempts = 0
         while True:
             try:
                 self.declare_queue(canonical_queue_name, ensure=True)

commit 76b0a76423fc7705b9d46a716645f4c0a01a6b1d

Fix off-by-one error introduced in #669
`attempts` variable is counting the number of attempts made.
Once this is >= to the max, raise Exception.
Also update log message to make this more obvious

dramatiq/brokers/rabbitmq.py
@@ -269,11 +269,11 @@ class RabbitmqBroker(Broker):
                 del self.connection
 
                 attempts += 1
-                if attempts > MAX_DECLARE_ATTEMPTS:
+                if attempts >= MAX_DECLARE_ATTEMPTS:
                     raise ConnectionClosed(e) from None
 
                 self.logger.debug(
-                    "Retrying declare due to closed connection. [%d/%d]",
+                    "Retrying declare due to closed connection. [%d/%d] attempts made so far.",
                     attempts,
                     MAX_DECLARE_ATTEMPTS,
                 )
@@ -365,11 +365,11 @@ class RabbitmqBroker(Broker):
                     self.queues.remove(q_name(queue_name))
 
                 attempts += 1
-                if attempts > MAX_ENQUEUE_ATTEMPTS:
+                if attempts >= MAX_ENQUEUE_ATTEMPTS:
                     raise ConnectionClosed(e) from None
 
                 self.logger.debug(
-                    "Retrying enqueue due to closed connection. [%d/%d]",
+                    "Retrying enqueue due to closed connection. [%d/%d] attempts made so far.",
                     attempts,
                     MAX_ENQUEUE_ATTEMPTS,
                 )